51295d97de2745d34b64b7965c6a1499d322cec3
vinutha.c <vinutha.c@WKSBAN24TR17015.corp.mphasis.com>
2019-08-08 09:55:55 +0530

    new dao added

---
+++ Izdoo/src/main/java/com/parv/izdoo/dao/CandidateDao.java
@@ -0,0 +1,11 @@
+package com.parv.izdoo.dao;
+
+import java.util.List;
+
+import com.parv.izdoo.entities.Candidate;
+
+public interface CandidateDao {
+	
+	public List<Candidate> getAllCandidate();
+
+}
